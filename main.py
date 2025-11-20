e10e1d376105915f615dd48914d9eb8c9513114b
MrGAN12009 <mrgan12009@gmail.com>
2025-11-20 18:38:19 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,70 @@
+## data types
+#int
+# name = 10
+# print(type(name))
+# name = "dfj"
+# float
+# name = 10.01
+# print(type(name))
+
+# str
+# name = "adfojdlkfjodf"
+# print(type(name))
+
+# boolean
+# name = True/False(1/0)
+
+
+## if
+# a = 10
+# b = 8
+#
+# if a > b:
+#     print("a > b")
+# elif a == b:
+#     print("a == b")
+# else:
+#     print("a < b")
+
+## loops
+
+# n = 0
+# target = 10
+
+# while n < target:
+#     n += 1
+#     if n == 5:
+#         continue
+#     print(n)
+#
+# print("end")
+
+
+#i in range(n) -> [0, n) с k = +1
+#i in range(n, m) -> [n, m) c k = +1
+#i in range(n, m, k) -> [n, m) c k = k
+# for i in range(10):
+#     print(i)
+
+# def main(name, age):
+#     print(f"Your name is {name} and your age is {age} years old")
+#
+# main(1, 1)
+# main(2, 2)
+# main(3, 3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
